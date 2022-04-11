1492e4b26fc778e472fce3f698ba4d4215be6ee3
Venus-Kavunza <venusmwende@gmail.com>
2022-04-11 12:38:45 +0300

    it will add the url patterns

--- award/admin.py
+++ award/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Posts,Ratings,UserProfile
 
 # Register your models here.
+
+admin.site.register(Posts)
+admin.site.register(Ratings)
+admin.site.register(UserProfile)
