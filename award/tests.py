a162969253efb92e7aabfee09669cb620a499eac
Venus-Kavunza <venusmwende@gmail.com>
2022-04-11 12:08:24 +0300

    it will add test class for the ratings class

--- award/tests.py
+++ award/tests.py
@@ -31,7 +31,7 @@ class PostsTest(TestCase):
         post = Posts.search_project('Django')
         self.assertTrue(len(post) < 1)
 
-class ProfileTest(TestCase):
+class UserProfileTest(TestCase):
     def setUp(self):
         self.user = User(username='venus', email='venus@gmail.com', password='newpassword')
         self.user.save()
@@ -44,3 +44,19 @@ class ProfileTest(TestCase):
 
     def test_delete_user(self):
         self.user.delete()
+
+class RatingsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='venus', email="venus@gmail.com")
+        self.post = Posts.objects.create(title='Django', photo='img.png', description='first project test',
+                                        user=self.user, url="http://django.com")
+        self.rating = Ratings.objects.create(design=9, usability=10, content=5, user=self.user, post=self.post)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Ratings))
+    def test_save_ratings(self):
+        self.rating.save_rating()
+        rating = Ratings.objects.all()
+        self.assertTrue(len(rating) > 0)        
+
+    
