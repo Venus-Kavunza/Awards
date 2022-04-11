bece7f80b0a1c8caa88bbc5f396b912fab51b9b8
Venus-Kavunza <venusmwende@gmail.com>
2022-04-11 17:12:06 +0300

    it will add the project function

--- award/views.py
+++ award/views.py
@@ -54,3 +54,55 @@ def user_profile(request, username):
     
     
     return render(request, 'all-awards/post.html', {'user_poster': user_poster,'user_posts':user_posts,'post_form':post_form,'current_user':current_user})
+
+@login_required(login_url='login')
+def project(request, post):
+    post = Posts.objects.get(title=post)
+    ratings = Ratings.objects.filter(user=request.user, post=post).first()
+    rating_status = None
+    current_user=request.user
+    
+
+    if request.method == "POST":
+        post_form = PostsForm(request.POST,request.FILES)
+        if post_form.is_valid():
+            post = post_form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return HttpResponseRedirect(reverse("home"))
+    else:
+        post_form = PostsForm()
+    
+    if ratings is None:
+        rating_status = False
+    else:
+        rating_status = True
+    if request.method == 'POST':
+        form = RatingsForm(request.POST)
+        if form.is_valid():
+            rate_result = form.save(commit=False)
+            rate_result.user = request.user
+            rate_result.post = post
+            rate_result.save()
+            post_ratings = Ratings.objects.filter(post=post)
+
+            design_rate = [d.design for d in post_ratings]
+            design_av = sum(design_rate) / len(design_rate)
+
+            usability_rate = [us.usability for us in post_ratings]
+            usability_av = sum(usability_rate) / len(usability_rate)
+
+            content_rate = [content.content for content in post_ratings]
+            content_av = sum(content_rate) / len(content_rate)
+
+            score = (design_av + usability_av + content_av) / 3
+            print(score)
+            rate_result.design_average = round(design_av, 2)
+            rate_result.usability_average = round(usability_av, 2)
+            rate_result.content_average = round(content_av, 2)
+            rate_result.score = round(score, 2)
+            rate_result.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = RatingsForm()
+    return render(request, 'all-awards/project.html', {'post': post,'rating_form': form,'rating_status': rating_status,'current_user':current_user,'post_form':post_form})
