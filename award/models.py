c53f70567fa32c83f6fcf42b042ea551634840c0
Venus-Kavunza <venusmwende@gmail.com>
2022-04-11 11:14:01 +0300

    it will add the posts and profile class

--- award/models.py
+++ award/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from phone_field import PhoneField
 
 # Create your models here.
+class Posts(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=300)
+    url = models.URLField(max_length=400)
+    photo = models.ImageField(upload_to='images/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    upload_date = models.DateTimeField(auto_now_add=True, blank=True)
+    technologies_used = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    bio = models.TextField(max_length=400, blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    profile_picture = models.ImageField(upload_to='images/',default='v1639327874/images/default_drurzc.jpg')
+    phone_number = PhoneField(max_length=15, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
