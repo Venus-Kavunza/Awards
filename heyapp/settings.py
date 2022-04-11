0ecaed924316ea2ec98719f4763c16e9c79ffe8c
Venus-Kavunza <venusmwende@gmail.com>
2022-04-11 15:33:25 +0300

    it will add the structure of the base.html file for the registration

--- heyapp/settings.py
+++ heyapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
