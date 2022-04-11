276c3e1235b74c8f363641356a2c5bac218a3b12
Venus-Kavunza <venusmwende@gmail.com>
2022-04-11 10:18:22 +0300

    it will add bootstrap to settings

--- heyapp/settings.py
+++ heyapp/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'award.apps.AwardsConfig',
+    'award',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
