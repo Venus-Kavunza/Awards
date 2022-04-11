c5049ab2dd3b6eb410668b5f866cb0b17f0490c6
Venus-Kavunza <venusmwende@gmail.com>
2022-04-11 11:08:45 +0300

    it will add database awards to the settings.py

--- award/views.py
+++ award/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-
+from django.http  import HttpResponseRedirect,Http404
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.contrib.auth.models import Use
+import random
 
 # Create your views here.
+
+
