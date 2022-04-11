d6cb810118bf0a852f9410e7f06aee8a5ba00d82
Venus-Kavunza <venusmwende@gmail.com>
2022-04-11 11:55:40 +0300

    it will add test class for posts

--- award/tests.py
+++ award/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Posts, Ratings
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class PostsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='venus', email="venus@gmail.com")
+        self.post = Posts.objects.create(title='Django', photo='img.png', description='first project test',
+                                        user=self.user, url="http://django.com")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Posts))
+    def test_save_post(self):
+        self.post.save_post()
+        post = Posts.objects.all()
+        self.assertTrue(len(post) > 0)        
+        
+    def test_search_post(self):
+        self.post.save()
+        post = Posts.search_project('Django')
+        self.assertTrue(len(post) > 0)
+
+    def test_get_posts(self):
+        self.post.save()
+        posts = Posts.all_posts()
+        self.assertTrue(len(posts) > 0)
+
+    def test_delete_post(self):
+        self.post.delete_post()
+        post = Posts.search_project('Django')
+        self.assertTrue(len(post) < 1)
--- award/urls.py
+++ award/urls.py
@@ -2,6 +2,8 @@ from django.urls import path,include
 from django.conf.urls import url
 from django.urls.conf import re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index, name='home'),
@@ -11,3 +13,6 @@ urlpatterns = [
     path('search/', views.search_project, name='search'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
